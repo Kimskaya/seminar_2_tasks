07b1b557031daae52f0665b0d1e576363f72c3fe
AnnaKim <annakim@inbox.ru>
2022-12-24 17:30:51 +0300

    task_10 completed

---
+++ task_10/Program.cs
@@ -0,0 +1,12 @@
+﻿//program takes 3-digit number and shows 
+//the second digit of this number
+int GetUserNumber(string message)
+{
+    Console.WriteLine(message);
+    int ThreeDigitNumber = int.Parse(Console.ReadLine()!); 
+    return ThreeDigitNumber;
+}
+int N= GetUserNumber("Input three-digit number");
+int Div1 = N/10;
+int Div2 = Div1%10;
+Console.WriteLine($" {Div2} is the second digit");
