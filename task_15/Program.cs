8f16ae1d8f375b6e95807d036367cb57b5a9f44d
AnnaKim <annakim@inbox.ru>
2022-12-24 17:32:54 +0300

    task_15 completed

---
+++ task_15/Program.cs
@@ -0,0 +1,24 @@
+﻿// program which takes a weekday and shows 
+//if its a weekend day or not
+int GetDayNumber(string message)
+{
+    Console.WriteLine(message);
+    int DayNumber = int.Parse(Console.ReadLine()!);
+    return DayNumber;
+}
+int Day = GetDayNumber("Input Day Number");
+    void ChechTheDay (int Day)
+    {if (Day <6)
+     {
+        Console.WriteLine("This is a weekday");
+     }
+    else if(Day>7)
+     {
+        Console.WriteLine("Incorrect number");
+     }
+     else 
+        {
+            Console.WriteLine("This is a Weekend day");
+        }  
+    }
+    ChechTheDay(Day);
